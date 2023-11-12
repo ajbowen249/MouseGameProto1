65265be8d1d3b112c229d1a4f2711bd00a9ecdd6
Alex Bowen <ajbowen249@gmail.com>
2023-11-12 05:09:09 -0500

    feat: use less energy with shovel

--- Assets/Scripts/Interactables/SnowBank.cs
+++ Assets/Scripts/Interactables/SnowBank.cs
@@ -13,9 +13,17 @@ public class SnowBank : MonoBehaviour
             return;
         }
 
-        player.ExpendEnergy(60);
-
-        HUD.Instance.AddMessage("You expend 60 energy clearing the snow with your bare hands.");
+        int shovels;
+        if (player.Inventory.TryGetValue("Shovel", out shovels))
+        {
+            player.ExpendEnergy(10);
+            HUD.Instance.AddMessage("You expend 10 energy clearing the snow with your shovel.");
+        }
+        else
+        {
+            player.ExpendEnergy(60);
+            HUD.Instance.AddMessage("You expend 60 energy clearing the snow with your bare hands.");
+        }
 
         Destroy(gameObject);
     }
