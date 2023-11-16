edb44db237bb39ee8f0a0e6c8c5a8105eae2acb1
Alex Bowen <ajbowen249@gmail.com>
2023-11-16 18:36:56 -0500

    feat: add neighbor interactions

---
+++ Assets/Scripts/GameCells/BlockedRoad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockedRoad : MonoBehaviour
+{
+    public GameObject SnowBank;
+
+    void Start()
+    {
+        var cell = gameObject.GetComponent<GameCell>();
+        cell.SetExitRequirement(() => {
+            if (SnowBank == null)
+            {
+                return true;
+            }
+
+            HUD.Instance.AddMessage("The snow and your neighbor block your path.");
+
+            return false;
+        });
+    }
+}
--- Assets/Scripts/NPCs/Neighbor1.cs
+++ Assets/Scripts/NPCs/Neighbor1.cs
@@ -1,14 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Neighbor1 : MonoBehaviour
 {
+    public GameObject PlowPath;
+    public GameObject ReturnPath;
+    public GameObject SnowBank;
+
     private NPC _npc;
 
-    void Start()
+    private MouseController _player;
+
+    void OnPlayerEnter(GameObject player)
     {
+        _player = player.GetComponent<MouseController>();
         _npc = gameObject.GetComponent<NPC>();
+
+        var clearCost = GetSnowClearCost(PlayerHasShovel());
+
         _npc.DialogTree = new DialogNode
         {
             Message = "Well hey, there neighbor! Can you help me clear out this here snow?",
@@ -16,30 +28,39 @@ public class Neighbor1 : MonoBehaviour
             {
                 new DialogOption
                 {
-                    Text = "Okay!",
+                    Text = "Sure thing!",
                     Tag = "positive",
-                    Node = null,
+                    Cost = clearCost,
                 },
                 new DialogOption
                 {
                     Text = "No way!",
-                    Tag = "",
                     Node = new DialogNode
                     {
-                        Message = "What the heck, for real?!",
+                        Message = "Are you sure? We could make light work of it together!",
                         Options = new List<DialogOption>
                         {
                             new DialogOption
                             {
                                 Text = "Fine",
                                 Tag = "positive",
-                                Node = null,
+                                Cost = clearCost,
                             },
                             new DialogOption
                             {
-                                Text = "Yeah, really!",
-                                Tag = "negative",
-                                Node = null,
+                                Text = "Yes, really.",
+                                Node = new DialogNode
+                                {
+                                    Message = "Well, okay. I guess just sit tight while I handle this.",
+                                    Options = new List<DialogOption>
+                                    {
+                                        new DialogOption
+                                        {
+                                            Text = "Ok.",
+                                            Tag = "negative",
+                                        },
+                                    }
+                                }
                             },
                         },
                     },
@@ -51,5 +72,47 @@ public class Neighbor1 : MonoBehaviour
     void OnInteraction(GameObject interactor)
     {
         _npc.InitiateDialog(interactor);
+    }
+
+    void OnDialogChoice(string tag)
+    {
+        if (string.IsNullOrEmpty(tag))
+        {
+            return;
+        }
+
+        PathFollower.SendObjectAlongPath(gameObject, PlowPath, () => {
+            if (tag == "positive")
+            {
+                _player.ExpendResources(GetSnowClearCost(PlayerHasShovel()));
+            }
+            else
+            {
+                _player.ExpendResources(new ActionCost
+                {
+                    description = "You watch your neighbor clear the snow alone",
+                    time = 2,
+                });
+            }
+
+            Destroy(SnowBank);
+
+            PathFollower.SendObjectAlongPath(gameObject, ReturnPath, () => {
+            });
+        });
+    }
+
+    private ActionCost GetSnowClearCost(bool hasShovel)
+    {
+        return hasShovel ?
+            new ActionCost { description = "You help clear the snow with your shovel", energy = 5, time = 2.5f / 60f } :
+            new ActionCost { description = "You help clear the snow with your bare hands", energy = 30, time = 1 };
+    }
+
+    private bool PlayerHasShovel()
+    {
+        int shovels;
+        _player.Inventory.TryGetValue("Shovel", out shovels);
+        return shovels > 0;
     }
 }
