24a60551d07876895a69c30fe554c574a0def675
Alex Bowen <ajbowen249@gmail.com>
2023-12-04 17:38:07 -0500

    feat: initial WFC logic

---
+++ Assets/Scripts/WFC/WFC.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PendingCell<TCell>
+{
+    public List<TCell> PossibleCells { get; set; }
+
+    private int _maxCellTypes;
+
+    public PendingCell(List<TCell> possibleCells)
+    {
+        PossibleCells = new List<TCell>(possibleCells);
+        _maxCellTypes = PossibleCells.Count;
+    }
+
+    public bool IsSettled()
+    {
+        return PossibleCells.Count <= 1;
+    }
+
+    public bool IsUntouched()
+    {
+        return PossibleCells.Count == _maxCellTypes;
+    }
+}
+
+
+public class WFCGrid<TCell>
+{
+    private List<List<PendingCell<TCell>>> _grid;
+
+    public int Rows { get; private set; }
+    public int Cols { get; private set; }
+
+    public WFCGrid(List<TCell> possibleCells, int cols, int rows)
+    {
+        Rows = rows;
+        Cols = cols;
+
+        _grid = new List<List<PendingCell<TCell>>>();
+        for (int row = 0; row < rows; row++)
+        {
+            var rowList = new List<PendingCell<TCell>>();
+
+            for (int col = 0; col < cols; col++)
+            {
+                rowList.Add(new PendingCell<TCell>(possibleCells));
+            }
+
+            _grid.Add(rowList);
+        }
+    }
+
+    /// <summary>
+    /// Gets cell by row and column. Returns null if the cell is off the grid.
+    ///
+    /// Note that grid size and coordinates are int, not uint, both because C# prefers int for indices, and for
+    /// convenience of relative addressing.
+    /// </summary>
+    public PendingCell<TCell> GetCell(int row, int col)
+    {
+        if (row < 0 || col < 0 || row > Rows - 1 || col > Cols - 1)
+        {
+            return null;
+        }
+
+        return _grid[row][col];
+    }
+
+    public List<((int, int), PendingCell<TCell>)> GetNeighborCells(int row, int col)
+    {
+        return new List<(int, int)>
+        {
+            (row - 1, col),
+            (row + 1, col),
+            (row, col - 1),
+            (row, col + 1),
+        }
+        .Select(pair => ((row, col), GetCell(pair.Item1, pair.Item2)))
+        .Where(pair => pair.Item2 != null)
+        .ToList();
+    }
+}
+
+/// <summary>
+/// Base Wave Function Collapse logic.
+///
+/// Specifically, it wraps up a WFCGrid and contains the logic to visit neighboring cells as needed after possible cell
+/// types are updated through SetCell. The general flow is to use additional logic to place initial cells, then flip-
+/// flop between calls to IterateComplete and additional external population logic until the grid is settled.
+///
+/// Note that external mutations to cells should be done through SetCell, which will enqueue pending neighbors for
+/// reduction. Types returned through the Reducer are automatically applied this way.
+/// </summary>
+public class WFCContext<TCell>
+{
+    /// <summary>
+    /// Given a cell, return an updated list of possible types (usually based on neighbors). Returning null means no
+    /// change.
+    /// </summary>
+    public delegate List<TCell> Reducer(int row, int col, PendingCell<TCell> cell);
+
+    public WFCGrid<TCell> Grid { get; private set; }
+
+    private Reducer _reducer;
+    private Queue<(int, int)> _queue = new Queue<(int, int)>();
+
+    public WFCContext(List<TCell> possibleCells, int rows, int cols, Reducer reducer)
+    {
+        Grid = new WFCGrid<TCell>(possibleCells, rows, cols);
+        _reducer = reducer;
+    }
+
+    public void SetCell(int row, int col, List<TCell> possibleCells, bool revisitSettled)
+    {
+        var cell = Grid.GetCell(row, col);
+        if (cell == null)
+        {
+            return;
+        }
+
+        cell.PossibleCells = possibleCells;
+        var neighbors = Grid.GetNeighborCells(row, col);
+        foreach (var neighbor in neighbors)
+        {
+            if (!neighbor.Item2.IsSettled() || revisitSettled)
+            {
+                (int neighborRow, int neighborCol) = neighbor.Item1;
+                QueueCell(neighborRow, neighborCol);
+            }
+        }
+    }
+
+    public void IterateComplete()
+    {
+        while (_queue.Count > 0)
+        {
+            Iterate();
+        }
+    }
+
+    private void Iterate()
+    {
+        (int row, int col) coord;
+        if (!_queue.TryDequeue(out coord))
+        {
+            return;
+        }
+
+        var cell = Grid.GetCell(coord.row, coord.col);
+        if (cell == null)
+        {
+            return;
+        }
+
+        var newTypes = _reducer(coord.row, coord.col, cell);
+        if (newTypes != null)
+        {
+            SetCell(coord.row, coord.col, newTypes, false);
+        }
+    }
+
+    private void QueueCell(int row, int col)
+    {
+        var pair = (row, col);
+        if (!_queue.Contains(pair))
+        {
+            _queue.Enqueue(pair);
+        }
+    }
+}
